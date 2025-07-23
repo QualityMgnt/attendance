02c0dfe10ceb5283dfac0fb4de0d044d9baee31c
QualityMgnt <iceacc95@gmail.com>
2025-07-23 14:56:33 +0300

    Update leave_tracker.js

--- leave_tracker.js
+++ leave_tracker.js
@@ -1,19 +1,42 @@
 // leave_tracker.js
 
-// Import necessary global variables and functions
-import { db, activeAgents, usersData, loggedInUserRole, todayGlobal, showPage, fetchAttendanceData, fetchAllUsersData, updateHomeStatistics, updateDashboardData } from './main.js';
+// Import necessary global variables and functions.
+// Ensure every variable used in this file is listed here.
+import {
+    db,
+    activeAgents,
+    usersData,
+    loggedInUserRole,
+    todayGlobal,
+    showPage,
+    fetchAttendanceData,
+    fetchAllUsersData,
+    updateHomeStatistics,
+    updateDashboardData,
+    currentLeaveTrackerDate, // This was missing in the original plan
+    formatDate, // You'll likely need this
+    markedDates,
+    attendanceData
+} from './main.js';
 
-let currentLeaveTrackerDate; // This will be managed by main.js
 let leavePlannerData = {}; // Cache for leave data
 
 export function initializeLeaveTracker(initialDate) {
     currentLeaveTrackerDate = initialDate;
     // You might also need to initialize some DOM elements here if they are not global
-    // DOM.prevMonthLeaveBtn.addEventListener(...)
+    DOM.prevMonthLeaveBtn.addEventListener('click', () => {
+        currentLeaveTrackerDate.setMonth(currentLeaveTrackerDate.getMonth() - 1);
+        fetchLeaveData(currentLeaveTrackerDate.getFullYear(), currentLeaveTrackerDate.getMonth());
+    });
+    DOM.nextMonthLeaveBtn.addEventListener('click', () => {
+        currentLeaveTrackerDate.setMonth(currentLeaveTrackerDate.getMonth() + 1);
+        fetchLeaveData(currentLeaveTrackerDate.getFullYear(), currentLeaveTrackerDate.getMonth());
+    });
 }
 
 export async function fetchLeaveData(year, month) {
     const monthYearKey = new Date(year, month).toLocaleString('en-US', { month: 'long', year: 'numeric' });
+    DOM.currentLeaveMonthYear.textContent = monthYearKey;
     try {
         const docRef = db.collection('leaves').doc(monthYearKey);
         const doc = await docRef.get();
@@ -32,14 +55,20 @@ export async function fetchLeaveData(year, month) {
 }
 
 export function renderLeavePlannerTable() {
-    // ... same code as your original `renderLeavePlannerTable` function ...
-    // Note: You will need to access global variables like `DOM` and `activeAgents` here.
-    // Make sure these are exported from `main.js` and imported here.
+    // ... your original renderLeavePlannerTable function code ...
+    // Note: It's assumed you will access DOM elements like `DOM.leaveTrackerTableContainer` here.
+    // If you haven't defined `DOM` as a global in main.js, this will also fail.
+    // The previous main.js code I provided does define `window.DOM`, so it should work.
+    
+    // ... (rest of the render function) ...
 }
 
 export async function updateLeaveStatus(agentName, fullDateKey, newStatus) {
-    // ... same code as your original `updateLeaveStatus` function ...
-    // Make sure you have access to `db`, `usersData`, `attendanceData`, etc.
+    // ... your original updateLeaveStatus function code ...
+    // This will need access to `db`, `usersData`, `attendanceData`, etc.
+    // which are now correctly imported.
+    
+    // ... (rest of the update function) ...
 }
 
 // Export any other functions needed by main.js
